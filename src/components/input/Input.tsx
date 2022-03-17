23285d2230340453ea98e88703d72f60efcdf6c3
Marek Tammevali <marek.tammevali@khk.ee>
2022-03-17 09:38:01 +0200

    added basic files.

---
+++ src/components/input/Input.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Input(placeholder: string) {
+    return (
+        <div>
+            <input placeholder={placeholder}/>
+
+        </div>
+    );
+}
+
+export default Input;
